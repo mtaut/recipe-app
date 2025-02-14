c12cbde98e3fe3dfb9e599e79bba96c9fcd5de26
mtaut <146488331+mtaut@users.noreply.github.com>
2025-02-13 23:24:29 -0500

    update

--- staticfiles/recipes/js/main.js
+++
@@ -1,7 +0,0 @@
-console.log("JavaScript file is loading!");
-
-function showMenu() {
-  const nav = document.getElementById("nav");
-  nav.classList.toggle("hide");
-  nav.classList.toggle("nav-show");
-}
